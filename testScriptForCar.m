cdecd38f730a4041798bf61990b63bfe4fa4d0b6
Dylan <67715553+CookOLo@users.noreply.github.com>
2024-11-30 14:03:22 -0700

    Add remote control when yellow is detected
    Also, reformat code for clearer reading

--- testScriptForCar.m
+++ testScriptForCar.m
@@ -1,65 +1,54 @@
-%Connections and Disconnects
-%clear all;
-%clear
-%color = brick.ColorCode(2); % Get Color on port 1.
-
-%display(color); % Print color code of the object.
-
-%Remote Control
+global key;
+InitKeyboard(); % Initializaes Keyboard prompting
 
 remote = false;
 auto = true;
 
-global key;
-
-
-InitKeyboard();
+previousColor = -1; 
 
 while true
+
+%Remote Control
 while remote == true
     pause(0.1);
 
     switch key
-        case 'p'
+        case 'p' % Move Picker Up
             disp('Up Arrow Pressed!');
-            brick.MoveMotorAngleAbs('B', -3, 0, 'Brake'); % Move Picker Up
-            %brick.WaitForMotor('B');
+            brick.MoveMotorAngleAbs('B', -3, 0, 'Brake'); 
             pause(0.1);
-            %brick.MoveMotor('B', 0);
         
-        case 'l'
+        case 'l' % Move Picker Down
             disp('Down Arrow Pressed!');
-            brick.MoveMotorAngleAbs('B', 3, 0, 'Brake'); % Move Picker Down
-            %brick.WaitForMotor('B');
+            brick.MoveMotorAngleAbs('B', 3, 0, 'Brake'); 
             pause(0.1);
-            %brick.MoveMotor('B', 0);
 
-        case 'leftarrow'
+        case 'leftarrow' % Stop Picker
             disp('Left Arrow Pressed!');
-            brick.MoveMotor('B', 0); % Stop Picker
+            brick.MoveMotor('B', 0); 
             brick.StopAllMotors('Brake');
 
-        case 'w'
-            brick.MoveMotor('A', -30);  % Forward motor A
-            brick.MoveMotor('D', -30);  % Forward motor D
+        case 'w' % Forward
+            brick.MoveMotor('A', -30);  
+            brick.MoveMotor('D', -30);  
 
-        case 's'
-            brick.MoveMotor('A', 30);  % Reverse motor A
-            brick.MoveMotor('D', 30);  % Reverse motor D
+        case 's' % Reverse
+            brick.MoveMotor('A', 30);  
+            brick.MoveMotor('D', 30);  
         
-        case 'a'
-            brick.MoveMotor('A', 30);   % Left motor moves backward
-            brick.MoveMotor('D', -50);  % Right motor moves forward
+        case 'a' % Left Turn
+            brick.MoveMotor('A', 30);   
+            brick.MoveMotor('D', -50);  
         
-        case 'd'
-            brick.MoveMotor('A', -50);% Left motor moves forward
-            brick.MoveMotor('D', 30);  % Right motor moves backward
+        case 'd' % Right Turn
+            brick.MoveMotor('A', -50);
+            brick.MoveMotor('D', 30);  
 
-        case 'space'
-            brick.MoveMotor('A', 0);  % Stop motor A
-            brick.MoveMotor('D', 0);  % Stop motor D
+        case 'space' % Stop Movement
+            brick.MoveMotor('A', 0);  
+            brick.MoveMotor('D', 0);  
         
-        case 'q'                      %Switches to Auto
+        case 'q' %Switches to Auto
             remote = false;
             auto = true;
             disp('Q Pressed');
@@ -71,26 +60,17 @@ while remote == true
     end
 end
 
-%TEMP
-
-
-
 %Auto Movement
 
-previousColor = -1; 
-
-
 while auto == true
-    % Check the touch sensor
     touch1324 = brick.TouchPressed(1);  % Reads touch sensor status
-    distance = brick.UltrasonicDist(4);
+    distance = brick.UltrasonicDist(4); % Reads distance
 
     
-    switch key                      % Switches back to remote
-   
-        case 'space'                %Stops EV3 Car
-            brick.MoveMotor('A', 0);  % Stop motor A
-            brick.MoveMotor('D', 0);  % Stop motor D
+    switch key % Switches back to remote
+        case 'space' %Stops EV3 Car
+            brick.MoveMotor('A', 0); 
+            brick.MoveMotor('D', 0);  
     end
     
 
@@ -104,21 +84,9 @@ while auto == true
         pause(.8);                 % Reverse for 1.8 seconds
         brick.MoveMotor('A', 0);
         brick.MoveMotor('D', 0);
-        %brick.StopAllMotors('Brake');  % Stop all motors
-        pause(.8); 
-        %{
-        brick.MoveMotor('A', 40);   % Left motor moves backward
-        brick.MoveMotor('D', -40);  % Right motor moves forward
-            
-        pause(.8);                 % Pause to complete a 90-degree turn
-        brick.MoveMotor('A', 0);
-        brick.MoveMotor('D', 0);
-        %brick.StopAllMotors('Brake');  % Stop all motors after turning
-        %}
-        %%
-        
+        pause(.8);
+
         %Left Turn Code
-        
         if distance > 45
             brick.MoveMotor('A', 40);   % Left motor moves backward
             brick.MoveMotor('D', -40);  % Right motor moves forward
@@ -126,9 +94,10 @@ while auto == true
             pause(.9);                 % Pause to complete a 90-degree turn
             brick.MoveMotor('A', 0);
             brick.MoveMotor('D', 0);  % Stop all motors after turning
+
         %Right Turn Code
         else
-            brick.MoveMotor('A', -40);% Left motor moves forward
+            brick.MoveMotor('A', -40); % Left motor moves forward
             brick.MoveMotor('D', 40);  % Right motor moves backward
             pause(.9);                 % Pause to complete a 90-degree turn
             brick.MoveMotor('A', 0);
@@ -137,7 +106,7 @@ while auto == true
         
         % Step 3: Resume forward movement
         disp("Resuming forward movement.");
-%}
+
     else
         % No obstacle detected - continue moving forward
         brick.MoveMotor('A', -50);  % Forward motor A
@@ -156,7 +125,7 @@ while auto == true
     
     if (color ~= previousColor) && (~isnan(color))
     %Testing with beeps
-        brick.MoveMotor('A', 0);  % Forward motor A
+        brick.MoveMotor('A', 0);  
         brick.MoveMotor('D', 0);
         if color == 5  % If red is found
            
@@ -166,31 +135,33 @@ while auto == true
         if color == 2  % If blue is found
             for i = 1:2 
                 brick.beep();
-                pause(.5); % Small pause between beeps
+                pause(.5); 
             end
-            remote = true;
+            remote = true; % Changes to remote control
             auto = false;
         end
 
-        if color == 3  %If green is found
+        if color == 3  % If green is found
             for i = 1:3
                 brick.beep();
-                pause(.5); % Small pause between beeps
+                pause(.5); 
             end
         end
 
         if color == 4  % If yellow is found
             for i = 1:4
                 brick.beep();
-                pause(.5); % Small pause between beeps
+                pause(.5); 
             end
+            remote = true; % Changes to remote control
+            auto = false;
         end
-        brick.MoveMotor('A', 0);  % Forward motor A
-        brick.MoveMotor('D', 0);
-        %brick.StopAllMotors('Brake');
+        brick.MoveMotor('A', 0); % Forward motor A
+        brick.MoveMotor('D', 0); % Forward motor D
         pause(1);
     end
-    previousColor = color;
+
+    previousColor = color; % Assigns current color as previous color
 
 end
 end
